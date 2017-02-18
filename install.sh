2a36fc63f6d9e898c0015410f2450b1aa68d1247
David Sperka <dsperka@cern.ch>
2017-02-18 09:36:47 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-git clone -b 80X https://github.com/VBF-HZZ/UFHZZAnalysisRun2.git
 git cms-merge-topic rafaellopesdesa:EgammaAnalysis80_EGMSmearer_Moriond17_23Jan
 cd EgammaAnalysis/ElectronTools/data
 git clone https://github.com/ECALELFS/ScalesSmearings.git
-git checkout Moriond17_23Jan_v1
-cd -
+cd ScalesSmearings
+git checkout tags/Moriond17_23Jan_v1
+cd ../../../../
 git cms-merge-topic cms-met:METRecipe_8020 -u
 git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
 cp UFHZZAnalysisRun2/Utilities/fixcrab.patch ZZMatrixElement
