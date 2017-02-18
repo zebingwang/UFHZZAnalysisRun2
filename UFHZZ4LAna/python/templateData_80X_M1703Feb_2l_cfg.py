d82992fa19d3ea9d9039ebc3a6a8b537c6183afb
David Sperka <dsperka@cern.ch>
2017-02-18 11:19:50 -0500

    udate data config for fixed met

--- UFHZZ4LAna/python/templateData_80X_M1703Feb_2l_cfg.py
+++ UFHZZ4LAna/python/templateData_80X_M1703Feb_2l_cfg.py
@@ -223,10 +223,44 @@ process.corrJets = cms.EDProducer ( "CorrJetsProducer",
 
 
 # Recompute MET
-#from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
-#runMetCorAndUncFromMiniAOD(process,
-#            isData=True,
-#            )
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+runMetCorAndUncFromMiniAOD(process,
+            isData=True,
+            )
+from PhysicsTools.PatUtils.tools.corMETFromMuonAndEG import corMETFromMuonAndEG
+corMETFromMuonAndEG(process,
+                    pfCandCollection="", #not needed
+                    electronCollection="slimmedElectronsBeforeGSFix",
+                    photonCollection="slimmedPhotonsBeforeGSFix",
+                    corElectronCollection="slimmedElectrons",
+                    corPhotonCollection="slimmedPhotons",
+                    allMETEGCorrected=True,
+                    muCorrection=False,
+                    eGCorrection=True,
+                    runOnMiniAOD=True,
+                    postfix="MuEGClean"
+                    )
+process.slimmedMETsMuEGClean = process.slimmedMETs.clone()
+process.slimmedMETsMuEGClean.src = cms.InputTag("patPFMetT1MuEGClean")
+process.slimmedMETsMuEGClean.rawVariation =  cms.InputTag("patPFMetRawMuEGClean")
+process.slimmedMETsMuEGClean.t1Uncertainties = cms.InputTag("patPFMetT1%sMuEGClean")
+del process.slimmedMETsMuEGClean.caloMET
+
+process.egcorrMET = cms.Sequence(
+    process.cleanedPhotonsMuEGClean+process.cleanedCorPhotonsMuEGClean+
+    process.matchedPhotonsMuEGClean + process.matchedElectronsMuEGClean +
+    process.corMETPhotonMuEGClean+process.corMETElectronMuEGClean+
+    process.patPFMetT1MuEGClean+process.patPFMetRawMuEGClean+
+    process.patPFMetT1SmearMuEGClean+process.patPFMetT1TxyMuEGClean+
+    process.patPFMetTxyMuEGClean+process.patPFMetT1JetEnUpMuEGClean+
+    process.patPFMetT1JetResUpMuEGClean+process.patPFMetT1SmearJetResUpMuEGClean+
+    process.patPFMetT1ElectronEnUpMuEGClean+process.patPFMetT1PhotonEnUpMuEGClean+
+    process.patPFMetT1MuonEnUpMuEGClean+process.patPFMetT1TauEnUpMuEGClean+
+    process.patPFMetT1UnclusteredEnUpMuEGClean+process.patPFMetT1JetEnDownMuEGClean+
+    process.patPFMetT1JetResDownMuEGClean+process.patPFMetT1SmearJetResDownMuEGClean+
+    process.patPFMetT1ElectronEnDownMuEGClean+process.patPFMetT1PhotonEnDownMuEGClean+
+    process.patPFMetT1MuonEnDownMuEGClean+process.patPFMetT1TauEnDownMuEGClean+
+    process.patPFMetT1UnclusteredEnDownMuEGClean+process.slimmedMETsMuEGClean)
 
 # Analyzer
 process.Ana = cms.EDAnalyzer('UFHZZ4LAna',
@@ -236,7 +270,7 @@ process.Ana = cms.EDAnalyzer('UFHZZ4LAna',
                               tauSrc      = cms.untracked.InputTag("slimmedTaus"),
                               jetSrc       = cms.untracked.InputTag("slimmedJets"),
                               mergedjetSrc = cms.untracked.InputTag("corrJets"),
-                              metSrc       = cms.untracked.InputTag("slimmedMETsMuEGClean"),
+                              metSrc       = cms.untracked.InputTag("slimmedMETsMuEGClean","","UFHZZ4LAnalysis"),
                               vertexSrc    = cms.untracked.InputTag("offlineSlimmedPrimaryVertices"),
                               beamSpotSrc  = cms.untracked.InputTag("offlineBeamSpot"),
                               conversionSrc  = cms.untracked.InputTag("reducedEgamma","reducedConversions"),
@@ -311,7 +345,8 @@ process.p = cms.Path(process.fsrPhotonSequence*
                      process.QGTagger*
                      #process.AK8PFJetCorrFactors*
                      #process.slimmedJetsAK8JEC*
-                     #process.fullPatMetSequence*
+                     process.fullPatMetSequence*
+                     process.egcorrMET*
                      process.corrJets*
                      process.Ana
                      )
