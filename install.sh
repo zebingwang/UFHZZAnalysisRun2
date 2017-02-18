17363de54d0924ca44f6b3e3f40cdc5ea420d6f5
David Sperka <dsperka@cern.ch>
2017-02-18 09:07:48 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-setenv SCRAM_ARCH slc6_amd64_gcc491
-cmsrel CMSSW_7_4_12_patch2
-cd CMSSW_7_4_12_patch2/src
-cmsenv
-git clone https://github.com/VBF-HZZ/UFHZZAnalysisRun2.git
-git clone -b 74x-root6 https://github.com/cms-analysis/HiggsAnalysis-CombinedLi
-mit.git HiggsAnalysis/CombinedLimit
-git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZM
-atrixElement
-cd ZZMatrixElement
-git checkout -b from-V00-02-01 V00-02-01
-cd ..
+git clone -b 80X https://github.com/VBF-HZZ/UFHZZAnalysisRun2.git
+git cms-merge-topic rafaellopesdesa:EgammaAnalysis80_EGMSmearer_Moriond17_23Jan
+cd EgammaAnalysis/ElectronTools/data
+git clone https://github.com/ECALELFS/ScalesSmearings.git
+git checkout Moriond17_23Jan_v1
+cd -
+git cms-merge-topic cms-met:METRecipe_8020 -u
+git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+cp UFHZZAnalysisRun2/Utilities/fixcrab.patch ZZMatrixElement
+(cd ZZMatrixElement ; git checkout -b from-v203 v2.0.4 ; git apply fixcrab.patch ; . setup.sh -j 12)
+git clone https://github.com/VBF-HZZ/KinZfitter.git
+git clone https://github.com/bachtis/Analysis.git -b KaMuCa_V4 KaMuCa
 scram b -j 8
