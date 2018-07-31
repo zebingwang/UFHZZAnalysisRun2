61fb8b7a1ed252bb63b02a5e063082c668f8cfc0
chenmingshui <mschen@cern.ch>
2018-07-31 13:12:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,7 @@ git cms-merge-topic -u perrozzi:HTXS_clean
 git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
 cp UFHZZAnalysisRun2/Utilities/fixcrab.patch ZZMatrixElement
 (cd ZZMatrixElement ; git checkout -b from-v204 v2.0.4 ; git apply fixcrab.patch ; . setup.sh -j 12)
+#NOTE need to copy MELA library from peviously compiled area (the permission to access cms-higgs.cern.ch is gone, somehow) 
+# e.g. scp from /raid/raid9/ahmad/CMSSW_8_0_26_patch1/src/src/ZZMatrixElement/MELA/data/slc6_amd64_gcc530/libmcfm_703.so 
+# or cp lxplus.cern.ch:~mschen/public/libmcfm_703.so-80X-v2.0.4 libmcfm_703.so
 scram b -j 8
